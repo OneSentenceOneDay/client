d51a7b1455db25644979ae2e848f56aea10205cd
sohy19 <sohy19@hufs.ac.kr>
2023-03-18 01:59:09 +0900

    style: modal position

--- src/pages/auth/Login/styled.ts
+++ src/pages/auth/Login/styled.ts
@@ -25,8 +25,8 @@ export const DialogBox = styled.dialog<{ page: string }>`
 	background-color: #ffffff;
 	z-index: 10000;
 
-	top: ${(props) => (props.page === "modal" ? "30%" : "0rem")};
-	// top: 0rem;
+	top: ${(props) => (props.page === "modal" ? "25%" : "0rem")};
+
 	button {
 		min-width: 25.375rem;
 		max-width: 25.375rem;
