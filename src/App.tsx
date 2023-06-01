5666fd13134cd318649b79a551ef790fd36b301f
sohy19 <sohy19@hufs.ac.kr>
2023-06-02 00:02:31 +0900

    feat: install Hotjar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import AppRouter from "./routes/AppRouter";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { CookiesProvider } from "react-cookie";
 import ReactGA from "react-ga";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 function App() {
 	const clientId: string = process.env.REACT_APP_CLIENT_ID!;
@@ -9,6 +11,15 @@ function App() {
 	const gaTrackingId: string = process.env.REACT_APP_GA_TRACKING_ID!; // 환경 변수에 저장된 추적ID 가져오기
 	ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
 	ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
+
+	const HJID: number = parseInt(process.env.HJID!);
+	const HJSV: number = parseInt(process.env.HJSV!);
+
+	useEffect(() => {
+		if (process.env.NODE_ENV !== "development") {
+			hotjar.initialize(HJID, HJSV);
+		}
+	}, []);
 
 	return (
 		<>
